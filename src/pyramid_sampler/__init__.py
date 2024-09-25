5f900cb3cb59844a44d33157d77980a5dbdad045
chavlin <chris.havlin@gmail.com>
2024-09-25 10:47:24 -0500

    initial cookiecutter

---
+++ src/pyramid_sampler/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2024 Chris Havlin. All rights reserved.
+
+pyramid_sampler: simple tools for creating image pyramids
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
